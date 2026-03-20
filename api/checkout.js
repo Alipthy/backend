a79fce225ed0d106f42e7d867a61c3551189f79f
Alipthy <josethe107@gmail.com>
2026-03-20 00:12:37 -0500

    Update checkout.js

--- api/checkout.js
+++ api/checkout.js
@@ -1,28 +1,30 @@
-import Stripe from "stripe";
+const Stripe = require("stripe");
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: "2023-10-16",
 });
 
-export default async function handler(req, res) {
-  if (req.method !== "POST") {
+module.exports = async function handler(req, res) {
+  if (req.method !== "GET") {
     return res.status(405).json({ error: "Method not allowed" });
   }
 
   try {
     const session = await stripe.checkout.sessions.create({
       mode: "subscription",
-      ui_mode: "embedded",
+  
       line_items: [
         {
           price: "price_1TCnmXQiNrsFVsySeZPdrB73",
           quantity: 1,
         },
       ],
-      return_url: "https://jointhenxtlvl.com",
+      success_url: "https://jointhenxtlvl.com",
+cancel_url: "https://jointhenxtlvl.com",
     });
 
     return res.status(200).json({
-      clientSecret: session.client_secret,
+  url: session.url,
+});
     });
   } catch (err) {
     return res.status(500).json({
